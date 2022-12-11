71c0a2ff2a31201ea95a7560f8ac3941ed88fee7
Ahmed Lebda <a.lebda.180@gmail.com>
2022-12-11 13:50:45 +0200

    Add scroll to top and hide nav on scrolling down

--- js/app.js
+++ js/app.js
@@ -13,11 +13,12 @@
  * 
 */
 
-// Global Variables
+// ===========> Global Variables <=========== 
+
 const pageSections = document.querySelectorAll("section")
 const navList = document.querySelector('#navbar__list')
 
-// build nav function
+// ===========> build nav function <=========== 
 
 const buildNav = (sections) => {
     // Make sure that there are sections in the page
@@ -40,7 +41,7 @@ const buildNav = (sections) => {
 }
 buildNav(pageSections);
 
-// Add class 'active' to section when near top of viewport
+// ===========> Add class 'active' to section when near top of viewport <=========== 
 function scrollActive() {
     pageSections.forEach(section => {
         if (section.getBoundingClientRect().top <= 50 && section.getBoundingClientRect().bottom > 50) {
@@ -54,7 +55,7 @@ function scrollActive() {
 }
 window.addEventListener('scroll', scrollActive)
 
-// Scroll to anchor ID using scrollTO event
+// ===========> Scroll to anchor ID using scrollTO event <=========== 
 
 function scrollToSection(e) {
     // preventing the default action of anchor element
@@ -64,7 +65,31 @@ function scrollToSection(e) {
     const targetSection = document.querySelector(sectionId)
     window.scrollTo({ top: targetSection.offsetTop - 20, left: 0, behavior: 'smooth' })
 }
-
 [...navList.children].forEach(section => {
     section.addEventListener('click', scrollToSection);
 });
+
+// ===========> Scroll to to button <=========== 
+
+function scrollTop() {
+    const scrollTop = document.querySelector("#scrollToTop");
+    window.scrollY >= 500
+        ? scrollTop.classList.add("show")
+        : scrollTop.classList.remove("show")
+
+    scrollTop.addEventListener('click', () => window.scrollTo({ top: 0, left: 0, behavior: "smooth" }))
+}
+window.addEventListener('scroll', scrollTop)
+
+// ===========> Hide nav on scrolling down  <=========== 
+
+let prevScroll = window.scrollY;
+window.addEventListener('scroll', () => {
+    let currentScroll = window.scrollY;
+    if (prevScroll < currentScroll) {
+        navList.style.display = 'none';
+    } else {
+        navList.style.display = 'block'
+    }
+    prevScroll = currentScroll
+})
