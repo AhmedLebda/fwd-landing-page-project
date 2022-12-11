0034bbd8cdd765304b312a65c3de83020ac360d3
Ahmed Lebda <a.lebda.180@gmail.com>
2022-12-11 13:09:39 +0200

    Build nav and add scrolling functionality

--- js/app.js
+++ js/app.js
@@ -13,50 +13,58 @@
  * 
 */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+// Global Variables
+const pageSections = document.querySelectorAll("section")
+const navList = document.querySelector('#navbar__list')
 
-/**
- * Define Global Variables
- * 
-*/
+// build nav function
 
+const buildNav = (sections) => {
+    // Make sure that there are sections in the page
+    if (sections) {
+        const fragment = document.createDocumentFragment();
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+        sections.forEach(section => {
+            const sectionName = section.dataset.nav;
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+            a.classList.add("menu__link")
+            a.href = `#${section.id}`
+            a.textContent = sectionName
+            li.appendChild(a)
+            fragment.appendChild(li)
+        });
 
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
+        navList.appendChild(fragment)
+    }
+}
+buildNav(pageSections);
 
 // Add class 'active' to section when near top of viewport
-
+function scrollActive() {
+    pageSections.forEach(section => {
+        if (section.getBoundingClientRect().top <= 50 && section.getBoundingClientRect().bottom > 50) {
+            section.classList.add('section__active')
+            document.querySelector(`a[href='#${section.id}']`).classList.add("link__active")
+        } else {
+            section.classList.remove('section__active')
+            document.querySelector(`a[href='#${section.id}']`).classList.remove("link__active")
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive)
 
 // Scroll to anchor ID using scrollTO event
 
+function scrollToSection(e) {
+    // preventing the default action of anchor element
+    e.preventDefault();
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+    const sectionId = e.target.getAttribute('href')
+    const targetSection = document.querySelector(sectionId)
+    window.scrollTo({ top: targetSection.offsetTop - 20, left: 0, behavior: 'smooth' })
+}
 
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+[...navList.children].forEach(section => {
+    section.addEventListener('click', scrollToSection);
+});
